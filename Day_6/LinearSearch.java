024f115538210f036fafa6db04d43822fe364eab
srijanisaDev <srijankumar770@gmail.com>
2026-02-20 19:04:38 +0530

    searching Techniques

---
+++ Day_6/LinearSearch.java
@@ -0,0 +1,26 @@
+package Day_6;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+        int arr[]= {10,50,40,20,30};
+        int search=21;
+        int flag=0;
+
+        for(int n:arr)
+        {
+            if(n==search)
+            {
+                flag=1;
+                break;
+            }
+        }
+
+        if(flag==1)
+            System.out.println("Found");
+        else
+            System.out.println("Not Found");
+
+    }
+
+}
